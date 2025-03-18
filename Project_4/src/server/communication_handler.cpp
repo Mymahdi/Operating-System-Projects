50b7d4d6ea39881098e3dfd9b75a87429167f3f2
Mahdi <khesali.m.mahdi@gmail.com>
2025-03-18 03:45:47 +0330

    Implement server socket creation and binding

--- Project_4/src/server/communication_handler.cpp
+++ Project_4/src/server/communication_handler.cpp
@@ -9,3 +9,28 @@
 using namespace std;
 
 CommunicationHandler::CommunicationHandler(int port) : serverPort(port) {}
+
+
+
+void CommunicationHandler::startListening() {
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        cerr << "Error creating socket." << endl;
+        return;
+    }
+
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(serverPort);
+
+    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+        cerr << "Binding failed." << endl;
+        close(serverSocket);
+        return;
+    }
+
+    listen(serverSocket, 5);
+    cout << "Server listening on port " << serverPort << "..." << endl;
+}
+
